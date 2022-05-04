ef2ca8edb66dadb05a088061d5bf319c0b1d600c
Gustavo Aca <galasdalas50@gmail.com>
2022-05-04 18:18:32 -0300

    Botoes concluidos

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -5,6 +5,7 @@ import thoughtImageUrl from '../../assets/thought.svg'
 import { useState } from 'react';
 import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
 import { FeedbackContentStep } from "./Steps/FeedbackContentsStep";
+import { FeedbackSucessStep } from './Steps/FeedbackSuccessStep';
 
 
 
@@ -38,22 +39,32 @@ export type FeedbackType = keyof typeof feedbackTypes;
 export function WidgetForm() {
 
   const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
+  const [feedbackSent, setFeedbackSent] = useState(false);
 
 function handleRestartFeedback(){
   setFeedbackType(null);
+  setFeedbackSent(false);
 }
 
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-      
-      {!feedbackType ? (
+      { feedbackSent ? (
+        <FeedbackSucessStep onFeedbackRestartRequested={handleRestartFeedback}/>
+      ) : (
+        <>
+        {!feedbackType ? (
         <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
 
       ) : (
-        <FeedbackContentStep feedbackType={feedbackType} 
+        <FeedbackContentStep
+         feedbackType={feedbackType} 
         onFeedbackRestartRequested={handleRestartFeedback}
+        onFeedbackSent={() => setFeedbackSent(true)}
         />
       )}
+        </>
+      )}
+      
 
       <footer className="text-xs text-neutral-400">
         Feito com ♥ por <a className="underline underline-offset-2" href="https://github.com/GustavoAca" target="_blank" >Gustavo Acácio</a>
